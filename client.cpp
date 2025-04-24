351781c271a4012b8520478d6fbc10539dd78a60
Dagor011 <68698528+Dagor011@users.noreply.github.com>
2025-04-24 15:14:09 +0300

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <winsock2.h> 
+#include <ws2tcpip.h> //  для IPv6 и getaddrinfo()
+#include<string>
+
+#include <thread>
+
+#include <ctime>
+
+
+#include<fstream>
+#include<map>
+#pragma comment(lib, "Ws2_32.lib") 
+
+
+int main()
+{
+	WSAData wsaData;
+
+	SOCKET  clientSocket;
+
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR) {
+		std::cerr << "socket not initializated\n";
+		return EXIT_FAILURE;
+	}
+
+	clientSocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+	if (clientSocket == INVALID_SOCKET) {
+		std::cerr << "error to create socket\n";
+		WSACleanup();
+		return EXIT_FAILURE;
+	}
+
+	return 0;
+}
+
